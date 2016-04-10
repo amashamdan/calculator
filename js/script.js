a1e225db646df89668405a8ffa3669cb6dd2f4b9
Amer Hamdan <amashamdan@gmail.com>
2016-04-10 23:18:41 +0300

    Feat: The four operators now operational.
    User can now add, subtract, multiply and divide numbers.
    Note: the CE button's class changed fron backspace to clear all.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,51 @@
+var operation;
+var number1;
+var secondNumber = false;
 $(document).ready(function(){
 	$(".number").click(function() {
-		if ($(".output").html() !== "0"){
-			$(".output").html($(".output").html() + $(this).html());
+		if (secondNumber == false){
+			if ($(".output").html() !== "0"){
+				$(".output").html($(".output").html() + $(this).html());
+			} else {
+				$(".output").html($(this).html());
+			}
 		} else {
 			$(".output").html($(this).html());
+			secondNumber = false;
+		}
+	})
+
+	$(".clear-all").click(function() {
+		$(".output").html(0);
+		operation = null;
+		secondNumber = false;
+		number1 = 0;
+	})
+
+	$(".clear").click(function() {
+		$(".output").html(0);
+	})
+
+	$(".operation").click(function() {
+		operation = $(this).html();
+		number1 = Number($(".output").html());
+		secondNumber = true;
+	})
+
+	$(".equal").click(function() {
+		switch (operation){
+			case "+":
+				$(".output").html(Number($(".output").html()) + number1);
+				break;
+			case "-":
+				$(".output").html(Number(number1 - $(".output").html()));
+				break;
+			case "x":
+				$(".output").html(Number($(".output").html()) * number1);
+				break;
+			case "/":
+				$(".output").html(Number(number1 / $(".output").html()));
+				break;
 		}
 	})
 })
